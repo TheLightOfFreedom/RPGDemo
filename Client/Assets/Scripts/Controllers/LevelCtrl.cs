499b9be7e3c2c70546ab9352a4a7ce4ce2bf993d
stalendp <stalendp@gmail.com>
2017-07-02 10:54:54 +0800

    ss

---
+++ Client/Assets/Scripts/Controllers/LevelCtrl.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelCtrl {
+
+    public static void enterLevel01() {
+        Application.LoadLevel("Level/demo1");
+    }
+
+
+}
---
+++ Client/Assets/Scripts/Modules/Main.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Main : MonoBehaviour {
+    public Button btnEnterGame;
+
+    private void Awake() {
+        btnEnterGame.onClick.AddListener(delegate {
+            LevelCtrl.enterLevel01();
+        });
+    }
+
+}
